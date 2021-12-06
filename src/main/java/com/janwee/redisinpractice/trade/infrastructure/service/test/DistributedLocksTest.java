d4c0ad1d6f9343d1692d448fe66765ee518fb630
janwee-sha <janwee_sha@outlook.com>
2021-12-06 22:48:49 +0800

    refactor.

--- src/main/java/com/janwee/redisinpractice/trade/infrastructure/service/test/DistributedLocksTest.java
+++ src/main/java/com/janwee/redisinpractice/trade/infrastructure/service/test/DistributedLocksTest.java
@@ -2,6 +2,7 @@ package com.janwee.redisinpractice.trade.infrastructure.service.test;
 
 import com.janwee.redisinpractice.trade.infrastructure.service.DistributedLocks;
 import com.janwee.redisinpractice.trade.infrastructure.service.JedisConfig;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,49 +17,54 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ContextConfiguration(classes = {JedisConfig.class})
 class DistributedLocksTest {
-    private static final String PREFIX = "lock:";
+    private static final String LOCK_KEY_PREFIX = "lock:";
+    private static final String TEST_LOCK_KEY = "testLock";
+    private static final int TEST_PARTITION_INDEX = 15;
     @Autowired
     private Jedis conn;
 
     @BeforeEach
-    void initialize() {
-        conn.select(15);
+    void setUp() {
+        conn.select(TEST_PARTITION_INDEX);
+        conn.del(LOCK_KEY_PREFIX + TEST_LOCK_KEY);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        conn.select(TEST_PARTITION_INDEX);
+        conn.del(LOCK_KEY_PREFIX + TEST_LOCK_KEY);
     }
 
     @Test
-    void testAcquireLock() {
+    void shouldAcquireLock() {
         System.out.println("Testing acquireLock...");
-        String lockName = "testLock";
-        conn.del(PREFIX + lockName);
-        assertNotNull(DistributedLocks.acquireLock(conn, lockName, 1000));
-        assertNull(DistributedLocks.acquireLock(conn, lockName, 1000));
+        assertNotNull(DistributedLocks.acquireLock(conn, TEST_LOCK_KEY, 1000));
+        assertNull(DistributedLocks.acquireLock(conn, TEST_LOCK_KEY, 1000));
     }
 
     @Test
-    void testAcquireLockWithTimeout() {
+    void shouldAcquireLockWithTimeout() {
         System.out.println("Testing acquireLockWithTimeout...");
-        String lockName = "testLock";
-        conn.del(PREFIX + lockName);
-        assertNotNull(DistributedLocks.acquireLockWithTimeout(conn, lockName, 1000, 3000));
-        assertNull(DistributedLocks.acquireLockWithTimeout(conn, lockName, 1000, 3000));
+        assertNotNull(DistributedLocks.acquireLockWithTimeout(conn, TEST_LOCK_KEY, 1000,
+                3000));
+        assertNull(DistributedLocks.acquireLockWithTimeout(conn, TEST_LOCK_KEY, 1000,
+                3000));
         try {
             TimeUnit.MILLISECONDS.sleep(3000);
-            assertNotNull(DistributedLocks.acquireLockWithTimeout(conn, lockName, 1000, 3000));
+            assertNotNull(DistributedLocks.acquireLockWithTimeout(conn, TEST_LOCK_KEY, 1000,
+                    3000));
         } catch (InterruptedException e) {
             //do nothing
         }
     }
 
     @Test
-    void testReleaseLock() {
+    void shouldReleaseLock() {
         System.out.println("Testing releaseLock");
-        String lockName = "testLock";
-        String lockKey = PREFIX + lockName;
-        conn.del(lockKey);
-        String identifier = DistributedLocks.acquireLock(conn, lockName, 1000);
+        String identifier = DistributedLocks.acquireLock(conn, TEST_LOCK_KEY, 1000);
         if (identifier != null) {
-            DistributedLocks.releaseLock(conn, lockName, identifier);
-            assertNotEquals(identifier, conn.get(lockKey));
+            DistributedLocks.releaseLock(conn, TEST_LOCK_KEY, identifier);
+            assertNotEquals(identifier, conn.get(TEST_LOCK_KEY));
         }
     }
 }
